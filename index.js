702d57db800f52567a7a6a80c3d931526eb40e89
Nahum <losond8@gmail.com>
2021-02-13 09:54:02 -0800

    Creaating and Inserting DOM Nodes Lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const el = document.getElementById('main');
+el.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = "Nahomie is the champion."
